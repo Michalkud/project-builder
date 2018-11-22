c86215ca8f3e75d2aefb6788295973d2ed7881d2
Michal Kudrnac <michalkud@gmail.com>
2018-11-22 20:14:32 +0100

    Rest endpoint which execute building script

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Simple node.js server which will build my projects.
 
-- [] on one script run building of project
+- [X] on one script run building of project
   - [X] git clone from origin to actual branch
   - [X] yarn command which builds project
   - [X] deletion of duplicated types
   - [X] prisma deploy
 
-- [] rest get endpoint which execute building script
-- [] react application which will show all active branches and if you will click on branch it will be builded and served to user
+- [X] rest endpoint which execute building script
+- [] github integration on merge to certain branch
   
   
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+
 // child process which will be executing scripts
 const exec = require('child_process').exec;
 
@@ -29,11 +33,7 @@ const execCommand = function(command, cwd) {
   });
 };
 
-
-
-
-
-(async function() {
+const buildProject = async function() {
   try {
     // git pull from origin to sandbox branch
     await execCommand(`git pull origin sandbox`, frontendDir);
@@ -49,11 +49,100 @@ const execCommand = function(command, cwd) {
   } catch(e) {
     console.error(e);
   }
-})()
+};
+
+const httpServer = http.createServer(async function(req, res) {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url,true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+  // Get the HTTP Method
+  const method = req.method.toLowerCase();
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the headers as an object
+  const headers = req.headers;
+
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data', function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end', async function() {
+    buffer += decoder.end();
+
+    // Choose the handler this request should go to. If one is not exist go to not found handler
+    const choosenHandler = 
+      typeof(router[trimmedPath]) !== undefined ?
+      router[trimmedPath] :
+      router.notFond
+
+    // Construct the data object to send to the handler
+    const data = {
+      'trimmedPath' : trimmedPath,
+      'queryStringObject' : queryStringObject,
+      'method' : method,
+      'headers' : headers,
+      'payload' : JSON.parse(buffer)
+    };
+
+    const result = await choosenHandler(data);
+    console.log(result);
+    // Use the status code called back by the handler, or default to 200
+    const statusCode = typeof result.statusCode === 'number' ? result.statusCode : 200;
+    // Use the payload called back by the handler, or default to an empty object
+    const payload = typeof result.payload === 'object' ? result.payload : {};
+    // Convert the payload to a string
+    const payloadString = JSON.stringify(payload);
+    
+    res.setHeader('Content-Type', 'application/json');
+
+    // Return the response
+    res.writeHead(statusCode);
+
+    // Log the request path
+    console.log('Return this response:', statusCode, payloadString);
+    res.end(payloadString);
+  })  
+
+});
+
+// Instantiate the HTTP server
+httpServer.listen(3003, function() {
+  console.log(`The http server is listening on port 3003`);
+});
+
+// Handler
+
+const buildHookHandler = async function(data) {
+  try {
+    const result = await buildProject();
+    return { 
+      statusCode: 200, 
+      payload: {
+        message: `Project succesfully builded`
+      } 
+    }
+  } catch (e) {
+    return { 
+      statusCode: 400,
+      payload: {
+        message: `Error occured: ${e}`
+      }
+    };
+  }
+}
 
 
+// Define a request router
+const router = {
+  'build-hook' : buildHookHandler
+}
 
-
-// deletion of duplicated types
-// prisma deploy
 
